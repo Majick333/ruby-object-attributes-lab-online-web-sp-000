cd0bc3e1f15e909ec721a353a44d6ee69a665688
Ryan M <rmccauleymusic@gmail.com>
2019-11-05 01:45:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 
 class Dog
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
+    def name
+      @this_dogs_name
+  end
+  def breed=(dogs_breed)
+    @this_dogs_breed = dogs_breed
+  end
+    def breed
+      @this_dogs_breed
+    end
 
   #def initialize(breed)
   #  @breed = breed
